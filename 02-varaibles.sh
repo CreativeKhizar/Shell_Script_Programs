7731a0fb3121a5d58ef5c754cc957eab2c286a24
khizar <creativekhizar1279@gmail.com>
2023-10-18 21:18:34 +0530

    variables file made

---
+++ 02-varaibles.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hi Suresh Good Morning"
+echo "$PERSON2: Good Morning Ramesh"
+echo "$PERSON1: How are you Suresh"
+echo "$PERSON2: I am good. How are you"
