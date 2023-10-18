03ef4ab523201f6d2ff334bd211bd1acb3839493
khizar <creativekhizar1279@gmail.com>
2023-10-18 21:33:46 +0530

    variables file made

---
+++ 05-variables.sh
@@ -0,0 +1,10 @@
+echo "Please Enter your Username"
+read Username
+
+echo "UserName Entered is $Username"
+
+echo "Please Enter your Password"
+
+read -s Password
+
+echo "Password Entered entered is $Password"
