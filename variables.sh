2bb907578504664129384286c758614c5e7c7738
khizar <creativekhizar1279@gmail.com>
2023-10-18 21:16:01 +0530

    variables file made

--- variables.sh
+++ variables.sh
@@ -3,4 +3,4 @@
 echo "Ramesh: Hi Suresh Good Morning"
 echo "Suresh: Good Morning Ramesh"
 echo "Ramesh: How are you Suresh"
-echoo "Suresh: I am good. How are you"
+echo "Suresh: I am good. How are you"
