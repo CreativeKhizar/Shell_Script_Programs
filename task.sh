8c70c1baef1374cc150cfb4d049357b03de91fa3
khizar <creativekhizar1279@gmail.com>
2023-10-20 19:37:56 +0530

    changes made

--- task.sh
+++ task.sh
@@ -21,7 +21,7 @@ VALIDATE()
 
 if [ $USERID -ne 0 ]
 then
-    echo "$R Error: Please Run the Script with root Access$N"
+    echo -e "$R Error: Please Run the Script with root Access$N"
     exit 1
 fi
 
